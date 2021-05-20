d84b2e3e6c69f57a038a87e5571e7ddf8027ed93
Laurent Bosteels <laurent.bosteels@gmail.com>
2021-05-20 12:28:27 +0200

    solved exercise 3 - 5

--- expert.php
+++ expert.php
@@ -27,8 +27,38 @@ new_exercise(3);
 // === Exercise 3 ===
 // This should echo ` "Debugged !" `, fix it so that that is the literal text echo'ed
 
-$str = '“Debugged ! Also very fun”';
+$str = "Debugged !  Also very fun";
 echo substr($str, 0, 10);
 
+new_exercise(4);
+// === Exercise 4 ===
+// Sometimes debugging code is just like looking up code and syntax...
+// The print_r($week) should give:  Array ( [0] => mon [1] => tues [2] => wednes [3] => thurs [4] => fri [5] => satur [6] => sun )
+// Look up whats going wrong with this code, and then fix it, with ONE CHARACTER!
 
-?>
+foreach ($week as &$day) {
+    $day = substr($day, 0, strlen($day)-3);
+}
+
+print_r($week);
+
+// You can pass a variable by reference to a function so the function can modify the variable.
+// https://www.php.net/manual/en/control-structures.foreach.php
+// In order to be able to directly modify array elements within the loop precede $value with &. 
+// In that case the value will be assigned by reference.
+
+new_exercise(5);
+// === Exercise 5 ===
+// The array should be printing every letter of the alfabet (a-z) but instead it does that + aa-yz
+// Fix the code so the for loop only pushes a-z in the array
+
+$arr = [];
+for ($letter = 'a'; $letter != 'aa'; $letter++) {
+    array_push($arr, $letter);
+}
+
+print_r($arr); // Array ([0] => a, [1] => b, [2] => c, ...) a-z alfabetical array
+
+// for(initialization; condition; increment)
+// $a <= $b 	Less than or equal to 	true if $a is less than or equal to $b.
+// $a != $b 	Not equal 	true if $a is not equal to $b after type juggling.
