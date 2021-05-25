0068da5cd9cbcc85edd4d2f27fc43f158ec13d53
Laurent Bosteels <laurent.bosteels@gmail.com>
2021-05-25 18:27:06 +0200

    solved last 2 exercises

--- expert.php
+++ expert.php
@@ -55,6 +55,7 @@ new_exercise(5);
 
 $arr = [];
 for ($letter = 'a'; $letter != 'aa'; $letter++) {
+    
     array_push($arr, $letter);
 }
 
@@ -149,3 +150,45 @@ echo login('john@example.be', 'dfgidfgdfg') . '<br>';
 //no access
 echo login('wrong@example.be', 'wrong') . '<br>';
 //you can change things again!
+
+new_exercise(9);
+function isLinkValid(string $link) {
+    $unacceptables = array('https:','.doc','.pdf', '.jpg', '.jpeg', '.gif', '.bmp', '.png');
+
+    foreach ($unacceptables as $unacceptable) {
+              // var_dump(strpos($link, $unacceptable));
+        if (strpos($link, $unacceptable) !== false) {
+            return 'Unacceptable Found<br />';
+        }
+    }
+    return 'Acceptable<br />';
+}
+//invalid link
+echo isLinkValid('http://www.google.com/hack.pdf');
+//invalid link
+echo isLinkValid('https://google.com');
+//VALID link
+echo isLinkValid('http://google.com');
+//VALID link
+echo isLinkValid('http://google.com/test.txt');
+
+// add echo to display result
+// Check value of strpos to not strict false
+// 0 also is true
+
+new_exercise(10);
+
+//Filter the array $areTheseFruits to only contain valid fruits
+//do not change the arrays itself
+$areTheseFruits = ['apple', 'bear', 'beef', 'banana', 'cherry', 'tomato', 'car'];
+$validFruits = ['apple', 'pear', 'banana', 'cherry', 'tomato'];
+//from here on you can change the code
+$count = count($areTheseFruits);
+for ($i=0; $i < $count; $i++) {
+    if (!in_array($areTheseFruits[$i], $validFruits)) {
+        unset($areTheseFruits[$i]);
+    }
+}
+var_dump($areTheseFruits);//do not change this
+
+// The count was decreasing so the loop exited too soon.
